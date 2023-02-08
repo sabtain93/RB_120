27eb2d5c4546c4256c7091f27cd92535d2071901
Muhammad Sabtain <sabtain40@gmail.com>
2023-02-08 18:03:37 +0500

    first commit

---
+++ good_dog.rb
@@ -0,0 +1,18 @@
+class GoodDog
+
+end
+
+
+sparky = GoodDog.new
+
+=begin
+in the above code we created an instance of the GoodDog class and stored
+it in the variable sparky
+
+We can say that sparky is an instance or an object of the class GoodDog
+
+The entire work flow of creating a new object or instance from a class is
+known as instantiation. We can say that we have instantiated an object called
+sparky from the class GoodDog.
+
+=end
